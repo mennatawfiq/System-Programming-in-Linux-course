659bdf14d13c070c524c7b88bf1aef1b9fcacaeb
menna <twmnh195@gmail.com>
2025-03-13 19:57:04 +0200

    Add pwd

---
+++ pwd.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/limits.h>
+
+const char buf[] = "Usage: pwd\n";
+
+int main(int argc, char** argv[]){
+	if(argc > 1)
+		write(1, buf, strlen(buf));
+
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	write(1, cwd, strlen(cwd));
+
+	return 0;
+}
