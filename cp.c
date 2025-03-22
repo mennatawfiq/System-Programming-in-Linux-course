29d0878e02e6b56ea5ac27e0c6c2031121e813ae
menna <twmnh195@gmail.com>
2025-03-22 23:40:27 +0200

    Add: cat, cp, echo, mv, pwd

---
+++ cp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define COUNT 1024
+
+int main(int argc, char** argv) {
+	
+	if(argc != 3){
+		printf("Usage: cp <file> <destination>");
+		exit(-1);
+	}
+
+	int fd_read = open(argv[1], O_RDONLY);
+	if(fd_read < 0){
+		printf("Failed to open the file for reading");
+		exit(-2);
+	}
+
+	int fd_write = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_write == -1) {
+		close(fd_read);
+		printf("Failed to open the file for writing");
+		exit(-3);
+	}
+
+	char buf[COUNT];
+	int bytes_read;
+	while((bytes_read = read(fd_read, buf, COUNT)) > 0){
+		if(write(fd_write, buf, bytes_read) < 0 ){
+			close(fd_read);
+			close(fd_write);
+			printf("Failed to write to file");
+			exit(-4);
+		}
+	}
+
+	close(fd_read);
+	close(fd_write);
+	return 0;
+}
---
+++ mv.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+const int COUNT = 1024;
+
+int main(int argc, char** argv){
+	if(argc != 3) {
+		printf("Usage: mv <old> <new>\n");
+		exit(-1);
+	}
+
+	int f1 = open(argv[1], O_RDONLY);
+	int f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (f2 == -1) {
+		close(f1);
+		printf("Failed to open the file for writing\n");
+		exit(-2);
+	}
+
+	char buf[COUNT];
+	int bytes_read;
+	while((bytes_read = read(f1, buf, COUNT)) > 0){
+		if(write(f2, buf, bytes_read) < 0 ){
+			close(f1);
+			close(f2);
+			printf("Failed to write to file\n");
+			exit(-3);
+		}
+	}
+
+	close(f2);
+
+	int f3 = unlink(argv[1]);
+	
+	if(f3 < 0) {
+		printf("failed to remove %s\n", argv[1]);
+		exit(-4);
+	}
+
+
+	return 0;
+}
