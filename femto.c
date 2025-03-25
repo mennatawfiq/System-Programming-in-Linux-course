4d5ed055eac25473a86224fa13c49382fec5cb36
menna <twmnh195@gmail.com>
2025-03-25 11:21:11 +0200

    Updat femtoshell

--- femto.c
+++ femto.c
@@ -4,28 +4,38 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int main(int argc, char** agrv) {
+int main(int argc, char **agrv)
+{
 
 	char *buf;
 	size_t szBuf = 0;
 
-	while(1){
+	while (1)
+	{
 		printf("Femto shell prompt > ");
-		if(getline(&buf, &szBuf, stdin) == -1){
+		if (getline(&buf, &szBuf, stdin) == -1)
+		{
 			break;
 		}
-		if(strcmp(buf, "exit\n") == 0){
+
+		if (strcmp(buf, "\n") == 0)
+		{
+			continue;
+		}
+		else if (strcmp(buf, "exit\n") == 0)
+		{
 			printf("Good Bye\n");
 			exit(0);
-		} else if (strncmp(buf, "echo", 4) == 0) {
-			char *echo = buf + 5;
-			if (write(1, echo, strlen(echo)) < 0)
-        		{	
-            			printf("Error while writing to stdout");
-           			exit(-1);
-        		}
 		}
-	
+		else if (strncmp(buf, "echo", 4) == 0)
+		{
+			char *echo = buf + 5;
+			printf("%s", echo);
+		}
+		else
+		{
+			printf("Invalid command\n");
+		}
 	}
 	return 0;
 }
